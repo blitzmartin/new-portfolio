78fda61dca7a602ee4d180984b928b98a8062f72
Annalisa De Martino <lizmartinmusic@gmail.com>
2022-03-23 17:20:27 +0100

    fetching 6 images randomly and adding to grid

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ hamburger.addEventListener('click', () => {
     navUL.classList.toggle('show');
 })
 
-const URL = "https://api.unsplash.com/photos/random?count=5&client_id=Z4VtZg5imv416TqNew-jUC7wGSEMVvWlWojNat291VQ"; // getting 5 images
+const URL = "https://api.unsplash.com/photos/random?count=6&client_id=Z4VtZg5imv416TqNew-jUC7wGSEMVvWlWojNat291VQ"; // getting 5 images
 
 fetch(URL)
     .then(function (response) {
@@ -17,13 +17,9 @@ fetch(URL)
 
     .then(function (data) {
         for (let i = 0; i < data.length; i++) {
-            console.log(data[i].urls.regular);
+            document.getElementsByClassName("grid-item")[i].src = data[i].urls.regular;;
         }
-        
-        /* const imgSrc = `http://openweathermap.org/img/w/${data.weather[0].icon}.png`;
-        document.querySelector("body > img").src = imgSrc;
-        return */
-    })
+     })
     .catch(function (err) {
         console.log("Something went wrong: " + err)
     })
